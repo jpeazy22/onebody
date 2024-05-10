61114116041328a26efe12b601a0ddb430157fb8
Joanie Smeester <jsmeester22@gmail.com>
2024-05-09 20:46:49 -0600

    updated header, massage section and footer for responsive design

--- src/App.js
+++ src/App.js
@@ -17,11 +17,16 @@ import rateText from './assets/headerText/fitnessRatesBlue.png';
 import scheduleText from './assets/headerText/scheduleTextBlue.png';
 import massageText from './assets/headerText/massageText.png';
 import taglineWhite from './assets/oneBody_tagline.png';
+import taglineBlue from './assets/taglineBlue.png';
 
 // Fitness Program Image Imports
 import fit1 from './assets/monthlyMember.png';
 import fit2 from './assets/nextLevel.png';
 import fit3 from './assets/personalTraining.png';
+import box1 from './assets/1.png';
+import box2 from './assets/2.png';
+import box3 from './assets/3.png';
+
 
 // Icon Image Imports
 import icon1 from './assets/icons/deadlift.png';
@@ -36,6 +41,9 @@ import carolyn from './assets/testimonials/carolynM.jpg';
 import cecile from './assets/testimonials/Dumbbells.jpg';
 import lawrence from './assets/testimonials/before_after.png';
 
+// Video Imports 
+import video from './assets/woman-working-out.mp4';
+import skyVideo from './assets/sky-background.mp4';
 
 function App() {
     return (
@@ -124,12 +132,16 @@ const Home = () => {
     };
     return (
         <div className="Home">
+            {/*<video autoPlay muted loop className="BackgroundVideo">
+                <source src={skyVideo} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>*/}
             <div className="Container">
                 <section>
                     <img src={whiteLogo} className="App-logo" alt="logo" />
                 </section>
                 <section>
-                    <img src={taglineWhite} className="Tagline" alt="tagline" />
+                    <img src={taglineBlue} className="Tagline" alt="tagline" />
                 </section>
                 <section className="Services">
                     <div className="ServiceList">
@@ -190,28 +202,26 @@ const Home = () => {
 } */}
 const Fitness = () => {
     return (
-        <div className="Fitness">
-            <section className="Scan">
-                <img src={titleText} className="imageTitle" alt="titleText" />
-            </section>
-            <section className="column">
-                <h2>What To Expect</h2>
-                <p>A group (functional) workout consisting of a blend of strength, cardio, and high intensity using various fitness equipment, body weight, and dumbbells.</p><br />
-                <p> All fitness levels are welcome into this supportive community backed by a passionate trainer. </p>
-            </section>
-            <section className="column">
-                <p>With NASM Certified Professional Trainer</p>
-                <h4>COACH GABRIEL GABALDON</h4>
-                <h2>
-                    <a href="https://onebody.pushpress.com/open/interested/usr_3716df7673660c" target="_blank" rel="noreferrer">Click Here</a> or Scan Below for 3 Free Classes
-                </h2>
-                <img src={qrCode} className="qrCode" alt="qrCode" />
+        <div className="FitnessContainer">
+            <section className="Fitness">
+                {/*<section className="Scan">
+                    <img src={titleText} className="imageTitle" alt="titleText" />
+                </section>*/}
+                <section className="column">
+                    <img src={box1} className="box" alt="titleText" />
+                </section>
+                <section className="column">
+                    <img src={box2} className="box" alt="titleText" />
+                </section>
+                <section className="column">
+                    <img src={box3} className="box" alt="titleText" />
+                </section>
             </section>
         
             <div className="Rates">
                 <div className="Container">
                     <section>
-                        <img src={rateText} className="imageTitle" alt="rateText" />
+                        <img src={titleText} className="imageTitle" alt="rateText" />
                     </section>
                     <section className="InfoBlock">
                         <img src={fit1} className="fitnessRateImage" alt="" />
@@ -292,88 +302,97 @@ const Fitness = () => {
         </div>
         )
     }
-            const Schedule = () => {
-                return (
-                    <div className="Schedule">
-                        <img src={scheduleText} className="scheduleTitle" alt="Schedule Title" />
-                        <section>
-                            <div className="dailySchedule">
-                                <h3>PETER PAN PARK / WESTMINSTER CENTER PARK</h3>
-                                <p className="border">4801 W. 92nd Ave.<br/>
-                                Westminster, CO 80031</p>
-                                <p>
-                                    <span className="day">Monday -</span>
-                                    <span className="times">5am</span> 
-                                    <span className="times">6:30am</span> 
-                                    <span className="times">9am</span> 
-                                    <span className="times">6pm</span>
-                                </p>
-                                <p>
-                                    <span className="day">Tuesday -</span>
-                                    <span className="times">5am</span> 
-                                    <span className="times">6:30am</span> 
-                                    <span className="times">9am</span> 
-                                    <span className="times">6pm</span>
-                                </p>
-                                <p>
-                                    <span className="day">Wednesday -</span>
-                                    <span className="times">5:15am</span>
-                                    <span className="times"></span>
-                                    <span className="times"></span>
-                                    <span className="times">6pm</span>
-                                </p>
-                                <p>
-                                    <span className="day">Thursday -</span>
-                                    <span className="times">5am</span> 
-                                    <span className="times">6:30am</span> 
-                                    <span className="times">9am</span> 
-                                    <span className="times">6pm</span>
-                                </p>
-                                <p className="border">
-                                    <span className="day">Friday -</span>
-                                    <span className="times">5:15am</span> 
-                                    <span className="times">6:30am</span>
-                                    <span className="times">9am</span>
-                                    <span className="times"></span>
-                                </p>
-                                <iframe 
-                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3067.537072759645!2d-105.05196448429264!3d39.86392097943769!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876b89c439559c29%3A0x41f6c336186e8a3b!2sWestminster%20Center%20Park!5e0!3m2!1sen!2sus!4v1714089580274" 
-                                width="400" height="300" style={{ border: 0 }} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade">
-                                </iframe>
-                            </div>
-                            <div className="dailySchedule">
-                                <h3>WESTMINSTER CITY PARK</h3>
-                                <p className="border">10455 Sheridan Blvd.<br/>
-                                Westminster, CO 80020</p>
-                                <p className="border">
-                                    <span className="day">Saturday -</span>
-                                    <span className="times"></span> 
-                                    <span className="times"></span> 
-                                    <span className="times">9am</span> 
-                                    <span className="times"></span>
-                                </p>
-                                <iframe 
-                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3061.3832043644225!2d-105.0589299!3d39.8880519!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876b898a5b759f83%3A0xb614968b3ea1dfa6!2sWestminster%20City%20Park!5e0!3m2!1sen!2sus!4v1714090185591!5m2!1sen!2sus" 
-                                width="400" height="300" style={{ border: 0 }} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
-                            </div>
-                        </section>
-                    </div>
-            )
-        }
+        const Schedule = () => {
+            return (
+                <div className="Schedule">
+                    <img src={scheduleText} className="scheduleTitle" alt="Schedule Title" />
+                    <section>
+                        <div className="dailySchedule">
+                            <h3>PETER PAN PARK / WESTMINSTER CENTER PARK</h3>
+                            <p className="border">4801 W. 92nd Ave.<br/>
+                            Westminster, CO 80031</p>
+                            <p>
+                                <span className="day">Monday -</span>
+                                <span className="times">5am</span> 
+                                <span className="times">6:30am</span> 
+                                <span className="times">9am</span> 
+                                <span className="times">6pm</span>
+                            </p>
+                            <p>
+                                <span className="day">Tuesday -</span>
+                                <span className="times">5am</span> 
+                                <span className="times">6:30am</span> 
+                                <span className="times">9am</span> 
+                                <span className="times">6pm</span>
+                            </p>
+                            <p>
+                                <span className="day">Wednesday -</span>
+                                <span className="times">5:15am</span>
+                                <span className="times"></span>
+                                <span className="times"></span>
+                                <span className="times">6pm</span>
+                            </p>
+                            <p>
+                                <span className="day">Thursday -</span>
+                                <span className="times">5am</span> 
+                                <span className="times">6:30am</span> 
+                                <span className="times">9am</span> 
+                                <span className="times">6pm</span>
+                            </p>
+                            <p className="border">
+                                <span className="day">Friday -</span>
+                                <span className="times">5:15am</span> 
+                                <span className="times">6:30am</span>
+                                <span className="times">9am</span>
+                                <span className="times"></span>
+                            </p>
+                            <iframe 
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3067.537072759645!2d-105.05196448429264!3d39.86392097943769!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876b89c439559c29%3A0x41f6c336186e8a3b!2sWestminster%20Center%20Park!5e0!3m2!1sen!2sus!4v1714089580274" 
+                            width="400" height="300" style={{ border: 0 }} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade">
+                            </iframe>
+                        </div>
+                        <div className="dailySchedule">
+                            <h3>WESTMINSTER CITY PARK</h3>
+                            <p className="border">10455 Sheridan Blvd.<br/>
+                            Westminster, CO 80020</p>
+                            <p className="border">
+                                <span className="day">Saturday -</span>
+                                <span className="times"></span> 
+                                <span className="times"></span> 
+                                <span className="times">9am</span> 
+                                <span className="times"></span>
+                            </p>
+                            <iframe 
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3061.3832043644225!2d-105.0589299!3d39.8880519!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876b898a5b759f83%3A0xb614968b3ea1dfa6!2sWestminster%20City%20Park!5e0!3m2!1sen!2sus!4v1714090185591!5m2!1sen!2sus" 
+                            width="400" height="300" style={{ border: 0 }} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+                        </div>
+                    </section>
+                </div>
+        )
+    }
         const Massage = () => {
             return (
                 <div className="Massage">
                     <section className="MassageHeader">
                         <img src={massageText} alt="massageText" />
                     </section>
-                    <section>
+                    <section className="column">
                         <h2>Your Expert in Personalized Therapeutic Massage</h2>
-                        {/*<p>
-                            Gabriel Gabaldon had a natural talent for massage long before he became a licensed therapist. After being frequently praised for his intuitive touch and perfect pressure, he pursued formal training at the renowned Denver School of Massage Therapy, graduating in May 2012. Gabriel honed his skills at Massage Envy from 2012 to 2018, where he earned the Mountain Regional Massage Therapist of the Year award in 2017. Following his distinguished tenure, he launched his own private practice.
-                        </p>*/}
                         <p>
-                            Today, Gabriel offers personalized massage sessions tailored to each client's needs, employing techniques like Sports Massage, Structural Integration, Swedish (relaxing or deep tissue), and Trigger Point Therapy. With Colorado being an active state, take advantage of routine maintenance whether you’re looking for a session for injury prevention, to increase performance to prepare for a competition, or simply to relax from the stressors of life, be confident in booking your massage at One Body today and experience the difference dedicated, professional care can make!
+                            At One Body, Gabriel provides tailored massage therapies including:
                         </p>
+                        <ul>
+                            <li>Sports Massage</li>
+                            <li>Structural Integration</li>
+                            <li>Swedish</li>
+                            <li>Trigger Point Therapy</li>
+                        </ul>
+                        <p>
+                            Ideal for Colorado’s active lifestyle, these sessions enhance performance, aid in injury prevention, and relieve stress.<br /> Experience professional care that makes a difference!
+                        </p>
+                        <h2>Location</h2>
+                        <p>11178 Huron Suite 7 (upstairs) Northglenn CO</p>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3060.881554769044!2d-104.9958001!3d39.899284099999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c747f74ca257b%3A0xcdd60abb44bc41a4!2sDream%20Intentions%20Wellness%20Studio!5e0!3m2!1sen!2sus!4v1714138557957!5m2!1sen!2sus" width="300" height="200" style={{ border: 0 }} allowfullscreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
                     </section>
                     <section className="column">
                         <h2>Massage Rates</h2>
@@ -384,12 +403,7 @@ const Fitness = () => {
                             <li>⁃	$140/ 2 hour </li>
                         </ul>
                         <p>•	10% off to my fitness clients, 15% if they rebook within a month of their previous session </p>
-                        <h2>Location</h2>
-                        <p>11178 Huron Suite 7 (upstairs) Northglenn CO</p>
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3060.881554769044!2d-104.9958001!3d39.899284099999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c747f74ca257b%3A0xcdd60abb44bc41a4!2sDream%20Intentions%20Wellness%20Studio!5e0!3m2!1sen!2sus!4v1714138557957!5m2!1sen!2sus" width="300" height="200" style={{ border: 0 }} allowfullscreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
-                    </section>
-                    <section className="column">
-                        <h2>Contact us using this form to book your appointment and discover our latest specials!</h2>
+                        <h2>Contact us to book your appointment and explore our exclusive offers!</h2>
                         <form className="ContactForm">
                             <label htmlFor="name">Name:</label>
                             <input type="text" id="name" name="name" required />
@@ -455,23 +469,10 @@ const Contact = () => {
         <div className="Contact">
             <div className="ContactContainer">
                 <div className="LeftColumn">
-                    <h2>Contact Us</h2>
-                    <form className="ContactForm">
-                        <label htmlFor="name">Name:</label>
-                        <input type="text" id="name" name="name" required />
-
-                        <label htmlFor="email">Email:</label>
-                        <input type="email" id="email" name="email" required />
-
-                        <label htmlFor="message">Message:</label>
-                        <textarea id="message" name="message" rows="4" required></textarea>
-
-                        <button type="submit">Send</button>
-                    </form>
-                </div>
-                <div className="RightColumn">
-                    <h2>Stay Connected</h2>
-                    <p>Follow us on social media for the latest updates and offers!</p>
+                    <h2><span>Contact Me
+                        <a href="https://onebody.pushpress.com/open/interested/usr_3716df7673660c" target="_blank" rel="noreferrer"> HERE</a>!
+                    </span></h2>
+                    <h2>Follow for updates!</h2>
                     <div className="SocialLinks">
                         <a href="https://www.facebook.com/gabriel.gabaldon.1" target="_blank" rel="noreferrer">
                             <img src={facebook} alt="facebook" className="social-icons" />
