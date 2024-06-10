3da945d2f0057f826c965ad99047c41b3b617a85
Joanie Smeester <jsmeester22@gmail.com>
2024-06-10 15:36:57 -0600

    testing contact form

--- src/App.js
+++ src/App.js
@@ -559,7 +559,7 @@ const Contact = () => {
                 </div>
                 <div className="column">
                     <h3>Contact us to book your appointment and explore our exclusive offers!</h3>
-                    <form name="contact v1" className="ContactForm" action="/success" netlify>
+                    <form name="contact v1" className="ContactForm" netlify>
                         <label htmlFor="name">Name:</label>
                         <input type="text" id="name" name="name" required />
                         
